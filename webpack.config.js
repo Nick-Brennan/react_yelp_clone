e66ce4a3e9b96fb4b8e409e2384ea9c8f14eeda6
Nick Brennan <nick_p_b@yahoo.com>
2016-06-13 12:57:48 -0700

    configuring postcss

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,18 @@ const dist = join(root, 'dist');
 const NODE_ENV = process.env.NODE_ENV;
 const isDev = NODE_ENV === 'development';
 
+const cssModuleNames = `${isDev ? '[path][name]__[local]__' : ''}[hash:base64:5]`;
+
 var config = getConfig({
 	in: join(__dirname, 'src/app.js'),
 	out: join(__dirname, 'dist'),
 	clearBeforeBuild: true
 });
+
+config.postcss = [].concat([
+		require('precss')({}),
+		require('autoprefixer')({}),
+		require('cssnano')({})
+	]);
 
 module.exports = config;
